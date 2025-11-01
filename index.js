59d5e6e5615696dca164a470a3617efb15b5091f
srijan <srijan.mart@gmail.com>
2025-11-01 07:14:39 +0530

    Feat: 1st integreation

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello, Devops!");
+console.log("I'm learning Devops tools and practices .");
---
+++ test.js
@@ -0,0 +1,7 @@
+console.log("Starting the Test Script");
+
+setTimeout(() => {
+    console.log("Testing .....");
+}, 3000);
+
+console.log("Test Script Completed");
